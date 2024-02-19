d27f924e50ebefc5d0cff9f1af84bedaa1c5c94b
Hal-ang <jpjgv05@gmail.com>
2024-02-19 13:00:26 +0900

    Initialize section 1 solutions

---
+++ section1/10부제.js
@@ -0,0 +1,4 @@
+function solution(day, arr) {}
+
+const arr = [25, 23, 11, 47, 53, 17, 33];
+console.log(solution(3, arr));
---
+++ section1/중복단어제거.js
@@ -0,0 +1,3 @@
+function solution(s) {}
+let str = ['good', 'time', 'good', 'time', 'student'];
+console.log(solution(str));
